40b4e779c88e8f83c9ddf159c25cae6a1f401c44
Gaurav Goel <grvgoel19@gmail.com>
2023-04-20 13:02:37 +0530

    adding polyfill library

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { multiply } from 'react-native-session-manager';
-import SessionManager from 'src/SessionManager';
 import { useRef, useState } from 'react';
 import * as SecureStore from 'expo-secure-store';
+import SessionManager from 'react-native-session-manager';
 
 global.Buffer = global.Buffer || Buffer;
 
 const scheme = "session_manager_sample";
+const SESSION_TIMEOUT = 86400;
 
 const styles = StyleSheet.create({
   container: {
@@ -27,7 +28,7 @@ export default function App() {
   var sessionManager = useRef<SessionManager>();
 
   React.useEffect(() => {
-    sessionManager.current = new SessionManager("", SecureStore);         
+    sessionManager.current = new SessionManager("", SecureStore, SESSION_TIMEOUT);         
     multiply(3, 7).then(setResult);
   }, []);
 
@@ -43,7 +44,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {createSessionResult !== null ? <Text>CreateSessionResult: {createSessionResult}</Text> : null}
+      {createSessionResult != null ? <Text>CreateSessionResult: {createSessionResult}</Text> : null}
       <Button title="Login with Web3Auth" onPress={createSession} />
     </View>
   );
